6d6c4cc5ff54ecf4768085edfcb6c2371bc2c45e
Mirabbos Egamberdiyev <mirabbosegamberdiyev7@gmail.com>
2024-02-16 01:19:15 +0500

    TalabaValidator tugadi

---
+++ CRM/Application/Common/Validators/TalabaValidator.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities.CRM;
+
+namespace Application.Common.Validators;
+
+public static class TalabaValidator
+{
+    public static bool IsValid(this Talaba talaba)
+        => talaba != null &&
+        !string.IsNullOrEmpty(talaba.FirstName) &&
+        !string.IsNullOrEmpty(talaba.LastName) &&
+        !string.IsNullOrEmpty(talaba.PhoneNumber);
+    public static bool IsExist(this Talaba talaba, List<Talaba> talabas)
+        => talabas.Any(t => t.FirstName == talaba.FirstName &&
+        t.LastName == talaba.LastName &&
+        t.PhoneNumber == talaba.PhoneNumber &&
+        t.TalabaGuruhDavomats == talaba.TalabaGuruhDavomats &&
+        t.TalabaGuruhs == talaba.TalabaGuruhs &&
+        t.TalabaTolov == talaba.TalabaTolov
+        );
+        
+}
