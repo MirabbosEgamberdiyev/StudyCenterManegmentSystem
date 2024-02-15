145f9a176c374f197a399c4d3858c793bcd47ccc
Mirabbos Egamberdiyev <mirabbosegamberdiyev7@gmail.com>
2024-02-16 01:29:15 +0500

    TalabaGuruhValidator Tugadi

---
+++ CRM/Application/Common/Validators/TalabaGuruhValidator.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities.CRM;
+
+namespace Application.Common.Validators;
+
+public static class TalabaGuruhValidator
+{
+    public static bool IsValid(this TalabaGuruh talabaGuruh)
+        => talabaGuruh != null &&
+        talabaGuruh.GuruhId > 0 &&
+        talabaGuruh.TalabaId > 0;
+    public static bool IsExist(this TalabaGuruh talabaGuruh, List<TalabaGuruh> talabaGuruhs)
+        => talabaGuruhs.Any(t => t.GuruhId == talabaGuruh.GuruhId &&
+                                 t.TalabaId == talabaGuruh.TalabaId &&
+                                 t.Talaba == talabaGuruh.Talaba &&
+                                 t.Guruh == talabaGuruh.Guruh);
+}
