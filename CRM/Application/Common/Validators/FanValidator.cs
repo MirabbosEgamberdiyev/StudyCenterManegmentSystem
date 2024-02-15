30d0eb829f4dff1a84dbaf4aa82e23d8c8e423f7
Mirabbos Egamberdiyev <mirabbosegamberdiyev7@gmail.com>
2024-02-16 00:56:08 +0500

    FanValidator

---
+++ CRM/Application/Common/Validators/FanValidator.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities.CRM;
+
+namespace Application.Common.Validators;
+
+public static class FanValidator
+{
+    public static bool IsValid(this Fan fan)
+    {
+        return fan != null &&
+               !string.IsNullOrEmpty(fan.FanName) &&
+               !string.IsNullOrEmpty(fan.FanDescription);
+    }
+    public static bool IsExist(this Fan fan, List<Fan> fans)
+    {
+        return fans.Any(f =>
+            f.FanName == fan.FanName &&
+            f.FanDescription == fan.FanDescription &&
+            f.Id != fan.Id &&
+            Enumerable.SequenceEqual(f.TeacherFans ?? Enumerable.Empty<TeacherFan>(),
+            fan.TeacherFans ?? Enumerable.Empty<TeacherFan>())
+
+        );
+    }
+}
